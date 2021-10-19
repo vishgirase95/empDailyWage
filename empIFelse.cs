2654bc682e11794f82ec319887fd12702c3b3961
vish <vishgirase95@gmail.com>
2021-10-19 22:48:05 +0530

    [vishal] add startEmpWage

---
+++ empIFelse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            Random num = new Random();
+            int genNum = num.Next(0, 1);
+            Console.WriteLine(genNum);
+        }
+    }
+}
